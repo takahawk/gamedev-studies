8cd4816d111c2211a1bf2f6e3a7b89dce2d98cd3
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2022-02-21 03:11:25 +0200

    Add cell

---
+++ tetris/Cell.gd
@@ -0,0 +1,17 @@
+tool
+extends Sprite
+
+export(String, "red", "blue", "green", "yellow", "cyan", "magenta") var color
+
+var textures = {
+	"red": preload("res://cell_red.png"),
+	"blue": preload("res://cell_blue.png"),
+	"green": preload("res://cell_green.png"),
+	"cyan": preload("res://cell_cyan.png"),
+	"yellow": preload("res://cell_yellow.png"),
+	"magenta": preload("res://cell_magenta.png")
+	}
+
+
+func _ready():
+	texture = textures[color]
