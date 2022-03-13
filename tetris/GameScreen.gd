86735ad256e52abd9a875e7a9478e4a7535e5171
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2022-03-13 13:23:06 +0200

    Add game over handler

--- tetris/GameScreen.gd
+++ tetris/GameScreen.gd
@@ -8,6 +8,15 @@ var lines_scores = {
 }
 
 var score: int = 0
+var game_is_over: bool = false
+
+func _input(event: InputEvent):
+	if not game_is_over:
+		return
+	if event is InputEventMouseButton:
+		if event.pressed:
+			# TODO: change to record score screen
+			get_tree().change_scene("res://MenuScreen.tscn")
 
 func _ready() -> void:
 	$HUD/Score.text = "SCORE " + str(score)
@@ -16,3 +25,8 @@ func _ready() -> void:
 func _on_Tetris_lines_destroyed(count):
 	score += lines_scores[count]
 	$HUD/Score.text = "SCORE " + str(score)
+
+
+func _on_Tetris_game_is_over():
+	game_is_over = true
+	$HUD/GameOver.visible = true
